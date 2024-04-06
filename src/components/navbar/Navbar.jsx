10ff488a02d0e944f48f95e281814dc40907cc2f
Fatih <fdonmez_1512@hotmail.com>
2024-04-06 15:53:31 +0300

    Added framer motion and navbar animation.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import "./navbar.scss";
+import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <nav className="navbar">
       {/*Sidebar*/}
       <div className="wrapper">
-        <span>Fatihdevelops</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Fatihdevelops
+        </motion.span>
         <div className="social">
           <a href="">
             <img src="/facebook.png" alt="" />
